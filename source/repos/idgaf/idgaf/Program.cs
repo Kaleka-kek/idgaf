a6a386947a8157e0421d18d95e1907dc20565b09
kaleka-kek <arman201005@gmail.com>
2020-09-03 09:32:14 +0300

    second commit

--- source/repos/idgaf/idgaf/Program.cs
+++ source/repos/idgaf/idgaf/Program.cs
@@ -6,7 +6,7 @@ namespace idgaf
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("lol!");
         }
     }
 }
