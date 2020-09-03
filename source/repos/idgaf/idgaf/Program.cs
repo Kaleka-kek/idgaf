3d0cd96acc00207a4c5b7f47b096cfd5ae707c12
kaleka-kek <arman201005@gmail.com>
2020-09-03 09:25:06 +0300

    first commit

---
+++ source/repos/idgaf/idgaf/Program.cs
@@ -0,0 +1,12 @@
+﻿    using System;
+
+namespace idgaf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
