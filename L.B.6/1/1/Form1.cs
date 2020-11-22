1991448563bd701d38ae65c63c2c1d96e168d170
Kaleka-kek <70683706+Kaleka-kek@users.noreply.github.com>
2020-11-22 18:24:46 +0200

    L.B.6
    L.B.6

---
+++ L.B.6/1/1/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1
+{
+    
+    public partial class Form1 : Form
+    {
+        public string FileName;
+        public string Dir;
+        AbstractHandler instance;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FileName = textBox3.Text;
+            Dir = textBox2.Text;
+
+            if (FileName.EndsWith(".txt"))
+                instance = new TXT(FileName, Dir);
+            else if (FileName.EndsWith(".doc"))
+                instance = new DOC(FileName, Dir);
+
+            MessageBox.Show(instance.CreateFile());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FileName = textBox3.Text;
+            Dir = textBox2.Text;
+
+            if (FileName.EndsWith(".txt"))
+                instance = new TXT(FileName, Dir);
+            else if (FileName.EndsWith(".doc"))
+                instance = new DOC(FileName, Dir);
+
+            textBox1.Text = instance.OpenFile();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            FileName = textBox3.Text;
+            Dir = textBox2.Text;
+
+            if (FileName.EndsWith(".txt"))
+                instance = new TXT(FileName, Dir);
+            else if (FileName.EndsWith(".doc"))
+                instance = new DOC(FileName, Dir);
+
+            MessageBox.Show(instance.EditFile(textBox1.Text));
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            base.Capture = false;
+            Message m = Message.Create(base.Handle, 161, new IntPtr(2), IntPtr.Zero);
+            this.WndProc(ref m);
+        }
+    }
+    abstract class AbstractHandler
+    {
+        public string FileName;
+        public string Dir;
+
+        public AbstractHandler(string filename, string dir)
+        {
+            FileName = filename;
+            Dir = dir;
+        }
+        public abstract string OpenFile();
+        public abstract string CreateFile();
+        public abstract string EditFile(string text);
+    }
+    class DOC : AbstractHandler
+    {
+        public DOC(string filename, string dir) : base(filename, dir)
+        {
+            FileName = filename;
+            Dir = dir;
+        }
+        public override string CreateFile()
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(Dir);
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+
+            using (FileStream filestream = new FileStream($"{Dir}/{FileName}", FileMode.OpenOrCreate))
+            {
+                return "Файл успешно создан";
+            }
+        }
+
+        public override string OpenFile()
+        {
+            FileInfo fileInf = new FileInfo($"{Dir}/{FileName}");
+            if (fileInf.Exists)
+            {
+                using (FileStream filestream = File.OpenRead($"{Dir}/{FileName}"))
+                {
+                    byte[] array = new byte[filestream.Length];
+                    filestream.Read(array, 0, array.Length);
+                    string text = System.Text.Encoding.Default.GetString(array);
+                    return text;
+                }
+            }
+            else
+                return "Файл не существует, сначала создайте файл";
+        }
+
+        public override string EditFile(string text)
+        {
+            FileInfo fileInf = new FileInfo($"{Dir}/{FileName}");
+            if (fileInf.Exists)
+            {
+                using (FileStream filestream = File.Create($"{Dir}/{FileName}"))
+                {
+                    byte[] array = System.Text.Encoding.Default.GetBytes(text);
+                    filestream.Write(array, 0, array.Length);
+                    return "Файл успешно записан";
+                }
+            }
+            else
+                return "Файл не существует, сначала создайте файл";
+
+        }
+    }
+    class TXT : AbstractHandler
+    {
+        public TXT(string filename, string dir) : base(filename, dir)
+        {
+            FileName = filename;
+            Dir = dir;
+        }
+        public override string CreateFile()
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(Dir);
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+
+            using (FileStream filestream = new FileStream($"{Dir}/{FileName}", FileMode.OpenOrCreate))
+            {
+                return "Файл успешно создан";
+            }
+        }
+
+        public override string OpenFile()
+        {
+            FileInfo fileInf = new FileInfo($"{Dir}/{FileName}");
+            if (fileInf.Exists)
+            {
+                using (FileStream filestream = File.OpenRead($"{Dir}/{FileName}"))
+                {
+                    byte[] array = new byte[filestream.Length];
+                    filestream.Read(array, 0, array.Length);
+                    string text = System.Text.Encoding.Default.GetString(array);
+                    return text;
+                }
+            }
+            else
+                return "Файл не существует, сначала создайте файл";
+        }
+
+        public override string EditFile(string text)
+        {
+            FileInfo fileInf = new FileInfo($"{Dir}/{FileName}");
+            if (fileInf.Exists)
+            {
+                using (FileStream filestream = File.Create($"{Dir}/{FileName}"))
+                {
+                    byte[] array = System.Text.Encoding.Default.GetBytes(text);
+                    filestream.Write(array, 0, array.Length);
+                    return "Файл успешно записан";
+                }
+            }
+            else
+                return "Файл не существует, сначала создайте файл";
+
+        }
+    }
+}
---
+++ L.B.6/2/2/Program.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Media;
+using System.Runtime.InteropServices;
+using _2;
+
+namespace _2
+{
+    interface IMedia
+    {
+        void Play();
+        void Pause();
+        void Stop();
+
+    }
+
+    interface IPlayable
+    {
+        void Play();
+        void Pause();
+        void Stop();
+    }
+
+    interface IRecordable
+    {
+        void Record();
+        void Pause();
+        void Stop();
+    }
+
+    class PlayerItem
+    {
+        public void Scan()
+        {
+            Console.WriteLine("Введите путь");
+            var path = Console.ReadLine();
+            int i = 0;
+            int pos;
+            List<String> mp3 = new List<string>();
+            foreach (var file in (Directory.EnumerateFiles(path,"*.mp3")))
+            {
+                FileInfo name = new FileInfo(file);
+                mp3.Add(name.Name);
+                Console.WriteLine(mp3[i]);
+                i++;
+            }
+            List<String> mkv = new List<string>();
+            foreach (string file in (Directory.EnumerateFiles(path, "*.mkv")))
+            {
+                FileInfo name = new FileInfo(file);
+                mkv.Add(name.Name);
+                Console.WriteLine(mkv[i]);
+                i++;
+            }
+            List<String> wav = new List<string>();
+            foreach (string file in (Directory.EnumerateFiles(path, "*.wav")))
+            {
+                FileInfo name = new FileInfo(file);
+                wav.Add(name.Name);
+                Console.WriteLine(wav[i]);
+                i++;
+            }
+            Console.WriteLine("Выберите формат файла(mp3,mkv,wav):");
+            string Pname = Console.ReadLine();
+            if (Pname == "mp3")
+            {
+                PMp3 pMp3 = new PMp3();
+                Console.WriteLine("Выберите файл(номер)");
+                pos = Int32.Parse(Console.ReadLine());
+                pos -= 1;
+                Console.WriteLine("Выбран файл"+mp3[pos]);
+                Console.WriteLine("Record,Pause or Stop");
+                string Action = Console.ReadLine();
+                if (Action == "Record")
+                {
+                    pMp3.Record();
+                }
+                if (Action == "Pause")
+                {
+                    pMp3.Pause();
+                }
+                if (Action == "Stop")
+                {
+                    pMp3.Stop();
+                }
+            }
+            if (Pname == "mkv")
+            {
+                PMkv pMkv = new PMkv();
+                Console.WriteLine("Выберите файл(номер)");
+                pos = Int32.Parse(Console.ReadLine());
+                pos -= 1;
+                Console.WriteLine("Выбран файл" + mkv[pos]);
+                Console.WriteLine("Record,Pause or Stop");
+                string Action = Console.ReadLine();
+                if (Action == "Record")
+                {
+                    pMkv.Play();
+                }
+                if (Action == "Pause")
+                {
+                    pMkv.Pause();
+                }
+                if (Action == "Stop")
+                {
+                    pMkv.Stop();
+                }
+            }
+            if (Pname == "wav")
+            {
+                PWav pWav = new PWav();
+                Console.WriteLine("Выберите файл(номер)");
+                pos = Int32.Parse(Console.ReadLine());
+                pos -= 1;
+                Console.WriteLine("Выбран файл" + wav[pos]);
+                Console.WriteLine("Record,Pause or Stop");
+                string Action = Console.ReadLine();
+                if (Action == "Record")
+                {
+                    pWav.Play();
+                }
+                if (Action == "Pause")
+                {
+                    pWav.Pause();
+                }
+                if (Action == "Stop")
+                {
+                    pWav.Stop();
+                }
+            }
+        }
+    }
+    class PMp3 : IRecordable
+    {
+        public void Record()
+        {
+            Console.WriteLine("Recording...");
+        }
+
+        public void Pause()
+        {
+            Console.WriteLine("Pause...");
+        }
+        public void Stop()
+        {
+            Console.WriteLine("Stop...");
+        }
+    }
+    class PMkv: IPlayable
+
+    {
+    public void Play()
+    {
+        Console.WriteLine("Play...");
+    }
+
+    public void Pause()
+    {
+        Console.WriteLine("Pause...");
+    }
+
+    public void Stop()
+    {
+        Console.WriteLine("Stop...");
+    }
+    }
+}
+class PWav: IMedia
+{
+        public void Play()
+        {
+            Console.WriteLine("Play...");
+        }
+        public void Pause()
+        {
+            Console.WriteLine("Pause...");
+        }
+        public void Stop()
+        {
+            Console.WriteLine("Stop...");
+        }
+    }
+class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            PlayerItem playerItem = new PlayerItem();
+            playerItem.Scan();
+            Console.ReadKey();
+        }
+    }
+
